13dfcad2146be427d3465aa5be8044a1c12fc104
Daniel Grigsby <dbg28@case.edu>
2018-09-23 16:14:30 -0400

    Add project base files and repo info

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='Postr',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT',
+    author="Adam Beck, Daniel Grigsby, Dominique Owens, "
+        "Rachel Pavlokovick, Tommy Lu",
+    author_email="dbg28@case.edu",
+    description="5yncr is a peer to peer file sync app",
+    url="https://github.com/dbgrigsby/Postr/",
+    project_urls={
+        "Bug Tracker": "https://github.com/dbgrigsby/Postr/issues",
+        "Source Code": "https://github.com/5yncr",
+    },
+    install_requires=[
+    ],
+)
