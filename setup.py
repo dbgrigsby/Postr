06fc49fb7cb6c0ab74a2eaee5e02cdfa99f1c4dc
Daniel Grigsby <dbg28@case.edu>
2018-09-23 16:19:36 -0400

    Update repository info

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
     author="Adam Beck, Daniel Grigsby, Dominique Owens, "
         "Rachel Pavlokovick, Tommy Lu",
     author_email="dbg28@case.edu",
-    description="5yncr is a peer to peer file sync app",
+    description="Postr is a Social Media Managemtn Tool",
     url="https://github.com/dbgrigsby/Postr/",
     project_urls={
         "Bug Tracker": "https://github.com/dbgrigsby/Postr/issues",
-        "Source Code": "https://github.com/5yncr",
+        "Source Code": "https://github.com/dbgrigsby/Postr",
     },
     install_requires=[
     ],
