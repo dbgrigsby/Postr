071fd0cf16219984d835f60c7538b1b400ae29c7
Daniel Grigsby <dbg28@case.edu>
2018-10-06 12:39:57 -0400

    Add typing to all files

--- postr/inserter.py
+++ postr/inserter.py
@@ -1,17 +1,19 @@
 import sqlite3
+from sqlite3 import Connection
+from typing import Optional
 
 
-def create_connection(db_file):
+def create_connection(db_file: str) -> Optional[Connection]:
     try:
         conn = sqlite3.connect(db_file)
         return conn
-    except Exception as e:
-        print("connection unestablished")
+    except Exception:
+        print('connection unestablished')
 
     return None
 
 
-def create_person(conn, first, last, social):
+def create_person(conn: Connection, first: str, last: str, social: str) -> None:
     cur = conn.cursor()
     cur.execute(
         """INSERT INTO Person(FirstName, LastName, SocialMedia)
@@ -20,7 +22,7 @@ def create_person(conn, first, last, social):
     conn.commit()
 
 
-def create_job(conn, comment, media_path, optional_text):
+def create_job(conn: Connection, comment: str, media_path: str, optional_text: str) -> None:
     cur = conn.cursor()
     cur.execute(
         """INSERT INTO Job(Comment, MediaPath, OptionalText)
@@ -29,7 +31,7 @@ def create_job(conn, comment, media_path, optional_text):
     conn.commit()
 
 
-def create_custom_job(conn, date, job_id):
+def create_custom_job(conn: Connection, date: str, job_id: str) -> None:
     cur = conn.cursor()
     cur.execute(
         """INSERT INTO CustomJob(CustomDate, Job_ID)
@@ -38,7 +40,14 @@ def create_custom_job(conn, date, job_id):
     conn.commit()
 
 
-def create_bio(conn, use_display, display_first, display_last, age, comment):
+def create_bio(
+        conn: Connection,
+        use_display: bool,
+        display_first: str,
+        display_last: str,
+        age: int,
+        comment: str,
+) -> None:
     cur = conn.cursor()
     cur.execute(
         """INSERT INTO Bio (UseDisplayNameInfo, DisplayFirstName,
