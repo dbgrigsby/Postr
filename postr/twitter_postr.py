df981caba566543e219041f55785493fb06e772f
adam.beck <akb93@case.edu>
2018-10-07 03:00:02 -0400

    Setup auth keys for twitter postr

---
+++ postr/twitter_postr.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access the twitter API.
+ACCESS_TOKEN = '1034473360628039680-OflwYTsKwd6xh1nzcRWje6veoHsBDY'
+ACCESS_TOKEN_SECRET = '5NqHxFb7ZfwiD07hbSkRq3c9AkyPnOWgnIo4ZUpwjKchZ'
+CONSUMER_KEY = 'SiPdFBFFoLyVOuThjWMMI0ozI'
+CONSUMER_SECRET = 'ocg9XwH4FYiz5IZXgxfD3Sowww7UDCSZ8rxLlyLwDvnhuaaHan'
