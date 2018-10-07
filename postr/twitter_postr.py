489f79d5766b16a4576aeb517c1a59a78180e5a4
adam.beck <akb93@case.edu>
2018-10-07 03:11:00 -0400

    Implement writer and tweet listener

--- postr/twitter_postr.py
+++ postr/twitter_postr.py
@@ -1,5 +1,50 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
 # Variables that contain the user credentials to access the twitter API.
-ACCESS_TOKEN = '1034473360628039680-OflwYTsKwd6xh1nzcRWje6veoHsBDY'
-ACCESS_TOKEN_SECRET = '5NqHxFb7ZfwiD07hbSkRq3c9AkyPnOWgnIo4ZUpwjKchZ'
-CONSUMER_KEY = 'SiPdFBFFoLyVOuThjWMMI0ozI'
-CONSUMER_SECRET = 'ocg9XwH4FYiz5IZXgxfD3Sowww7UDCSZ8rxLlyLwDvnhuaaHan'
+ACCESS_TOKEN: str = '1034473360628039680-OflwYTsKwd6xh1nzcRWje6veoHsBDY'
+ACCESS_TOKEN_SECRET: str = '5NqHxFb7ZfwiD07hbSkRq3c9AkyPnOWgnIo4ZUpwjKchZ'
+CONSUMER_KEY: str = 'SiPdFBFFoLyVOuThjWMMI0ozI'
+CONSUMER_SECRET: str = 'ocg9XwH4FYiz5IZXgxfD3Sowww7UDCSZ8rxLlyLwDvnhuaaHan'
+
+
+class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets
+    """
+
+    def __init__(self):
+        pass
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        listener = Writer(fetched_tweets_filename)
+        auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        stream = Stream(auth, listener)
+
+        # This line filter Twitter Streams to capture data by the keywords:
+        stream.filter(track=hash_tag_list)
+
+
+class Writer(StreamListener):
+    """
+    A basic listener for real time hashtags
+    """
+
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+
+    def on_dat(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print('Error on data %s' % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
