1ab25bfb4254b842934e88a1fd7a440887d458e1
adam.beck <akb93@case.edu>
2018-10-08 20:23:41 -0400

    Refactor tags

--- postr/twitter_postr.py
+++ postr/twitter_postr.py
@@ -54,8 +54,8 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
     # Authenticate using config.py and connect to Twitter Streaming API.
-    hash_tag_list = ['politics']
+    tags = ['politics']
     fetched_tweets_filename = 'tweets.txt'
 
     twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    twitter_streamer.stream_tweets(fetched_tweets_filename, tags)
