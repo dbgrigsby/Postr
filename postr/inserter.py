0ceb695bedfc4c4c3802883dd940516a3ecec692
adam.beck <akb93@case.edu>
2018-10-10 00:31:50 -0400

    Docstrings for dbsetup and inserter. Fix SQL insertion for bios

--- postr/inserter.py
+++ postr/inserter.py
@@ -1,9 +1,11 @@
+""" Inserts rows into the database tables regarding scheduling operations """
 import sqlite3
 from sqlite3 import Connection
 from typing import Optional
 
 
 def create_connection(db_file: str) -> Optional[Connection]:
+    """Creates and returns a connection to a given database file """
     try:
         conn = sqlite3.connect(db_file)
         return conn
@@ -14,6 +16,7 @@ def create_connection(db_file: str) -> Optional[Connection]:
 
 
 def create_person(conn: Connection, first: str, last: str, social: str) -> None:
+    """Inserts a person/user into the database Person table; generates a unique ID """
     cur = conn.cursor()
     cur.execute(
         """INSERT INTO Person(FirstName, LastName, SocialMedia)
@@ -23,6 +26,8 @@ def create_person(conn: Connection, first: str, last: str, social: str) -> None:
 
 
 def create_job(conn: Connection, comment: str, media_path: str, optional_text: str) -> None:
+    """Creates a scheduled job/task for media operations.
+       comment and media path can be null """
     cur = conn.cursor()
     cur.execute(
         """INSERT INTO Job(Comment, MediaPath, OptionalText)
@@ -32,6 +37,7 @@ def create_job(conn: Connection, comment: str, media_path: str, optional_text: s
 
 
 def create_custom_job(conn: Connection, date: str, job_id: str) -> None:
+    """Creates a custom job/task, that is, a one-time job on a specific date """
     cur = conn.cursor()
     cur.execute(
         """INSERT INTO CustomJob(CustomDate, Job_ID)
@@ -47,12 +53,16 @@ def create_bio(
         display_last: str,
         age: int,
         comment: str,
+        website: str,
         person_id: int,
 ) -> None:
+    """ Creates a Bio for an associated user on a given platform
+        Used to store all saved users' bios, and can be used to retrieve any user
+        bio that meets a certain condition (e.g. contains a specific handle) """
     cur = conn.cursor()
     cur.execute(
         """INSERT INTO Bio (UseDisplayNameInfo, DisplayFirstName,
-                DisplayLastName, Age, Comment, Person_ID) VALUES(?, ?, ?, ?, ?)""",
-        (use_display, display_first, display_last, age, comment, person_id),
+                DisplayLastName, Age, Comment, Website, Person_ID) VALUES(?, ?, ?, ?, ?, ?)""",
+        (use_display, display_first, display_last, age, comment, website, person_id),
     )
     conn.commit()
