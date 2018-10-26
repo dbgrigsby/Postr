4aac9da98f7b4090d7a9b564fc68d652121e4472
adam.beck <akb93@case.edu>
2018-10-26 19:54:43 -0400

    Tests csv col reading

--- tests/AI_test.py
+++ tests/AI_test.py
@@ -15,12 +15,14 @@ def test_polarity() -> None:
 
 
 def test_sentiment() -> None:
+    # Tests the creation of the blobfile
     t.blobfile = os.path.join('tests/twitter/blobtest.csv')
     t.analyzeSentiment()
 
     my_file = Path(t.blobfile)
     assert my_file.is_file()
 
+    # Tests the numerical sentiment conversion in the blobfile
     scores = []
 
     with open(t.blobfile, 'r') as csvFile:
@@ -29,8 +31,20 @@ def test_sentiment() -> None:
 
         for row in reader:
             if i > 0:
-                scores.append(int(row[1]))
+                scores.append(float(row[1]))
             i += 1
 
     total = sum(scores)
     assert total != 0
+
+
+def test_read_csv_col() -> None:
+    t.blobfile = os.path.join('tests/twitter/blobtest.csv')
+    col = t.read_csv_col(1, t.blobfile)
+
+    for elem in col:
+        try:
+            to_float = float(elem)
+            assert isinstance(to_float, float)
+        except Exception:
+            assert False
