ed37a4a0a39b66ebb8d53cfeafba91cba2d3d2cd
adam.beck <akb93@case.edu>
2018-11-02 03:09:59 -0400

    Implement reader scanner

--- postr/schedule/reader.py
+++ postr/schedule/reader.py
@@ -1,5 +1,7 @@
 from datetime import datetime as dt
 import sqlite3
+from typing import List
+from typing import Any
 
 
 class Reader():
@@ -21,3 +23,24 @@ class Reader():
     def now(cls) -> int:
         """ Returns the current time """
         return int(dt.now().timestamp())
+
+    def scan_custom_jobs(self, seconds: int = 30) -> List[Any]:
+        """ Scans jobs every 'seconds' seconds, and returns a JSON
+            object representing any jobs to be operated on """
+        lower = self.schedule_range(seconds)
+        upper = self.now()
+
+        self.cursor.execute(f"""SELECT * FROM CustomJob WHERE
+                CustomJob.CustomDate BETWEEN {lower} and {upper}""")
+
+        json = [{
+            self.cursor.description[i][0]: value
+            for i, value in enumerate(row)
+        } for row in self.cursor.fetchall()]
+
+        return json
+
+    def schedule_range(self, seconds: int) -> int:
+        """ Returns the lower bound for a scheduled range """
+        now = self.now()
+        return now - seconds
