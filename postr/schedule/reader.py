128531b83dcda6be75a223d0669add56514b54c1
adam.beck <akb93@case.edu>
2018-11-01 23:19:01 -0400

    Refactor reader

---
+++ postr/schedule/reader.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+class Reader():
+    """
+    Continuously reads the database for scheduled operations.
+    Returns any scheduled operations that need to be ran.
+    """
+
+    def __init__(self) -> None:
+        file_path: str = 'postr/schedule/master_schedule.sqlite'
+        self.conn = sqlite3.connect(file_path, check_same_thread=False)
+        self.cursor = self.conn.cursor()
+
+    def cleanup(self) -> None:
+        """ Closes the database connection"""
+        self.conn.close()
