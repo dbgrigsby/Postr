c216eb3f44de4d3956e2e378f831f9a9b34d0e57
adam.beck <akb93@case.edu>
2018-12-08 18:13:50 -0500

    class automation page progress

--- postr/main2.py
+++ postr/main2.py
@@ -1,13 +1,13 @@
 import tkinter
-from tkinter import ttk
+from tkinter.ttk import Notebook
 
 # Widgets
-from tkinter import Tk, Label, Listbox
+from tkinter import Tk, Listbox, Frame, Button
 
 # Locations
-from tkinter import LEFT, RIGHT, TOP, MULTIPLE, END
+from tkinter import LEFT, TOP, MULTIPLE, END, BOTTOM
 
-# Misc
+# Misc Widgets
 from tkinter.scrolledtext import ScrolledText
 import tkinter.messagebox
 from tkinter import filedialog
@@ -19,66 +19,76 @@ def setup(main_gui: Tk) -> Tk:
     return main_gui
 
 
-def open_file_dialog() -> None:
-    filename = filedialog.askopenfilename(
-        initialdir='/', title='Select a file',
-        filetypes=(('jpeg', '*.jpg'), ('png', '*.png'), ('All files', '*.*')),
-    )
+def setup_apis(page: Frame) -> Listbox:
+    page_l = Frame(page)
+    page_l.pack(side=LEFT, padx=20)
 
-    print(filename)
+    api_box = Listbox(page_l, selectmode=MULTIPLE, width=15, height=8, font=('Times', 16))
+    apis = ['Facebook', 'Tumblr', 'Instagram', 'Twitter', 'Reddit', 'Youtube', 'Discord', 'Slack']
+    list(map(lambda x: api_box.insert(END, x), apis))
+    api_box.pack(side=LEFT)
+    return api_box
 
 
-def setup_interface_page(nb: ttk.Notebook) -> None:
-    def setup_apis(page: ttk.Frame) -> None:
-        api_box = Listbox(page, selectmode=MULTIPLE, width=15, height=8)
-        apis = ['Facebook', 'Tumblr', 'Instagram', 'Twitter', 'Reddit', 'Youtube', 'Discord', 'Slack']
-        list(map(lambda x: api_box.insert(END, x), apis))
-        api_box.pack(side=LEFT)
+def setup_text_box(page: Frame) -> ScrolledText:
+    comment_area = ScrolledText(master=page, wrap=tkinter.WORD, width=40, height=6)
+    comment_area.insert(tkinter.INSERT, 'Enter your comment here')
+    comment_area.pack(side=TOP, anchor='n', pady=20, expand=True)
+    return comment_area
 
-    def setup_methods(page: ttk.Frame) -> None:
-        method_box = Listbox(page, width=15, height=8)
-        methods = ['Post Text', 'Post Picture']
-        list(map(lambda x: method_box.insert(END, x), methods))
-        method_box.pack(side=RIGHT)
 
-    def setup_text_box(page: ttk.Frame) -> None:
-        comment_area = ScrolledText(master=page, wrap=tkinter.WORD, width=40, height=6)
-        comment_area.insert(tkinter.INSERT, 'Enter your comment here')
-        comment_area.pack(side=TOP, anchor='n', pady=20, expand=True)
+class GUI():
+    def __init__(self) -> None:
+        # setupmain()
+        self.root = setup(Tk())
+        self.gui = Notebook(self.root)
 
-    def setup_file_dialog(page: ttk.Frame) -> None:
-        ttk.Button(page, text='Browse a file', command=open_file_dialog).pack(side=TOP)
+        # Setup the posting page
+        page1 = Frame(self.gui)
+        page1.pack()
 
-        file_label = Label(page, text='', width=30)
-        file_label.pack(side=TOP)
+        self.api_box = setup_apis(page1)
+        self.setup_automation_buttons(page1)
+        # self.text_box = setup_text_box(page1)
+        # self.file_button = self.setup_file_dialog(page1)
+        # self.filepath = ''
+        self.gui.add(page1, text='Post Automation')
 
-    page = ttk.Frame(nb)
-    page.pack()
+        # Setup the analytics page
+        page2 = Frame(self.gui)
+        text = ScrolledText(page2)
+        text.pack(expand=1, fill='both')
+        self.gui.add(page2, text='Two')
 
-    setup_apis(page)
-    setup_methods(page)
-    setup_file_dialog(page)
-    setup_text_box(page)
+        # Pack out gui
+        self.gui.pack(expand=1, fill='both')
 
-    nb.add(page, text='Post Automation')
+    def setup_automation_buttons(self, page: Frame) -> None:
+        page_l = Frame(page)
+        page_l.pack(side=LEFT)
+
+        Button(page_l, text='Post text', command=self.post_text).pack(side=TOP)
+        Button(page_l, text='Post picture w/text', command=self.post_pic).pack(side=BOTTOM, anchor='s')
+
+    def post_text(self) -> None:
+        pass
+
+    def post_pic(self) -> None:
+        pass
+
+    def setup_file_dialog(self, page: Frame) -> Button:
+        b = Button(page, text='Browse a file', command=self.open_file_dialog)
+        b.pack(side=TOP)
+        return b
+
+    def open_file_dialog(self) -> None:
+        filename = filedialog.askopenfilename(
+            initialdir='/', title='Select a file',
+            filetypes=(('jpeg', '*.jpg'), ('png', '*.png'), ('All files', '*.*')),
+        )
+        self.filepath = filename
 
 
 if __name__ == '__main__':
-    # setupmain()
-    root = setup(Tk())
-    gui = ttk.Notebook(root)
-
-    # Setup the interface page for post automation
-    setup_interface_page(gui)
-
-    # Setup the analytics page
-
-    # second page
-    page2 = ttk.Frame(gui)
-    text = ScrolledText(page2)
-    text.pack(expand=1, fill='both')
-    gui.add(page2, text='Two')
-
-    gui.pack(expand=1, fill='both')
-
-    root.mainloop()
+    g = GUI()
+    g.root.mainloop()
