bc4d586ef1abbc8908116760d9058c204173406a
adam.beck <akb93@case.edu>
2018-12-09 00:07:42 -0500

    Abstract page1 to it's own class

--- postr/main2.py
+++ postr/main2.py
@@ -22,7 +22,7 @@ twitter = Twitter()
 
 def setup(main_gui: Tk) -> Tk:
     main_gui.title('Postr')
-    main_gui.geometry('800x400')
+    main_gui.geometry('700x400')
 
     # a fix for running on OSX - to center the title text vertically
     if main_gui.tk.call('tk', 'windowingsystem') == 'aqua':  # only for OSX
@@ -44,25 +44,27 @@ def setup_apis(page: Frame) -> Listbox:
     return api_box
 
 
+def api_iterator(box: Listbox) -> Iterator[str]:
+    api_cache = {
+        0: 'Facebook', 1: 'Tumblr', 2: 'Instagram', 3: 'Twitter',
+        4: 'Reddit', 5: 'Youtube', 6: 'Discord', 7: 'Slack',
+    }
+    selected_as_codes = box.curselection()
+    api_list = [api_cache[code] for code in selected_as_codes]
+
+    for api in api_list:
+        yield api
+
+
 class GUI():
     def __init__(self) -> None:
         # setup the gui
         self.root = setup(Tk())
         self.gui = Notebook(self.root)
 
-        # Misc variables for widgets
-        self.filepath = StringVar()
-        self.filepath.set('No file uploaded')
-        self.io_error = StringVar()
-        self.io_error.set('')
-
         # Setup the posting page
         page1 = Frame(self.gui)
-
-        self.api_box = setup_apis(page1)
-        self.setup_automation_buttons(page1)
-        self.text_box = self.setup_text_box(page1)
-        self.setup_IO_buttons(page1)
+        self.posting_frame = PostingPage(page1)
         self.gui.add(page1, text='Posting')
 
         # Setup the scheduling ppage
@@ -80,17 +82,18 @@ class GUI():
         # Pack out gui
         self.gui.pack(expand=1, fill='both')
 
-    def setup_text_box(self, page: Frame) -> ScrolledText:
-        page_r = Frame(page)
-        page_r.pack(side=RIGHT)
 
-        comment_area = ScrolledText(master=page_r, wrap=tkinter.WORD, width=40, height=6, bg='grey')
-        comment_area.insert(tkinter.INSERT, 'Enter your comment here')
-        comment_area.pack()
+class PostingPage():
+    def __init__(self, frame: Frame) -> None:
+        # Misc variables for widgets
+        self.filepath = StringVar()
+        self.filepath.set('No file uploaded')
+        self.io_error = StringVar()
+        self.io_error.set('')
 
-        Button(page_r, text='Upload file', command=self.open_file_dialog).pack(side=TOP, anchor='s')
-        Label(page_r, textvariable=self.filepath).pack(side=TOP, anchor='s')
-        return comment_area
+        self.api_box = setup_apis(frame)
+        self.setup_automation_buttons(frame)
+        self.text_box = self.setup_text_box(frame)
 
     def setup_automation_buttons(self, page: Frame) -> None:
         page_l = Frame(page)
@@ -99,24 +102,6 @@ class GUI():
         Button(page_l, text='Post text', command=self.post_text).pack(side=TOP)
         Button(page_l, text='Post picture w/text', command=self.post_pic).pack(side=TOP, anchor='s')
         Label(page_l, textvariable=self.io_error, fg='red').pack(side=BOTTOM, anchor='s')
-
-    def setup_IO_buttons(self, page: Frame) -> None:
-        page_r = Frame(page)
-        page_r.pack(side=RIGHT)
-
-        Button(page_r, text='Clear comment', command=self.clear_comment).pack(side=TOP, anchor='s')
-        Button(page_r, text='Clear file', command=self.clear_file).pack(side=TOP, anchor='s')
-
-    def api_iterator(self) -> Iterator[str]:
-        api_cache = {
-            0: 'Facebook', 1: 'Tumblr', 2: 'Instagram', 3: 'Twitter',
-            4: 'Reddit', 5: 'Youtube', 6: 'Discord', 7: 'Slack',
-        }
-        selected_as_codes = self.api_box.curselection()
-        api_list = [api_cache[code] for code in selected_as_codes]
-
-        for api in api_list:
-            yield api
 
     def post_text(self) -> None:
         text = str(self.text_box.get(1.0, END)).strip()
@@ -128,7 +113,7 @@ class GUI():
         self.io_error.set('')
 
         try:
-            for api in self.api_iterator():
+            for api in api_iterator(self.api_box):
                 if api == 'Twitter':
                     twitter.post_text(text)
                 else:
@@ -147,7 +132,7 @@ class GUI():
         self.io_error.set('')
 
         try:
-            for api in self.api_iterator():
+            for api in api_iterator(self.api_box):
                 if api == 'Twitter':
                     twitter.post_photo(url, text)
                 else:
@@ -155,18 +140,28 @@ class GUI():
         except Exception:
             self.io_error.set('An API failed to post a photo')
 
-    def clear_comment(self) -> None:
-        self.text_box.delete('1.0', END)
-        self.text_box.insert('1.0', 'Enter your comment here')
+    def setup_text_box(self, page: Frame) -> ScrolledText:
+        page_r = Frame(page)
+        page_r.pack(side=RIGHT)
 
-    def clear_file(self) -> None:
-        self.filepath.set('No file uploaded')
+        comment_area = ScrolledText(master=page_r, wrap=tkinter.WORD, width=40, height=6, bg='grey')
+        comment_area.insert(tkinter.INSERT, 'Enter your comment here')
+        comment_area.pack()
+
+        Button(page_r, text='Upload file', command=self.open_file_dialog).pack(side=TOP, anchor='s')
+        Label(page_r, textvariable=self.filepath).pack(side=TOP, anchor='s')
+
+        Button(page_r, text='Clear', command=self.clear_file).pack()
+        return comment_area
 
     def open_file_dialog(self) -> None:
         self.filepath.set(filedialog.askopenfilename(
             initialdir='/', title='Select a file',
             filetypes=(('jpeg', '*.jpg'), ('png', '*.png'), ('All files', '*.*')),
         ))
+
+    def clear_file(self) -> None:
+        self.filepath.set('No file uploaded')
 
 
 if __name__ == '__main__':
